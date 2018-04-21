b460ae91eee6f0da84ae9de66e963dc7d7a35ab1
Andy Aylward <aaylward@gmail.com>
2018-04-21 11:40:08 -0400

    hit

--- js/app.js
+++ js/app.js
@@ -2,12 +2,14 @@
   const canvas = document.querySelector("#pong");
   const ctx = canvas.getContext("2d");
   const ball = {x: 140, y: 100, dx: 2, dy: 2, r: 10};
-  const us = {x: canvas.width - 7, y: canvas.height / 2 - 30, color: "red"};
-  const them = {x: 0, y: canvas.height / 2 - 30, color: "green"};
+  const paddleHeight = 60;
+  const paddleStart = (canvas.height - paddleHeight) / 2;
+  const us = {x: canvas.width - 7, y: paddleStart, color: "red"};
+  const them = {x: 0, y: paddleStart, color: "green"};
 
   const drawPlayer = (p) => {
     ctx.beginPath();
-    ctx.rect(p.x, p.y, 7, 60);
+    ctx.rect(p.x, p.y, 7, paddleHeight);
     ctx.fillStyle = p.color;
     ctx.fill();
     ctx.closePath();
@@ -30,8 +32,16 @@
     drawPlayer(them);
   }
 
+  const weHit = () => {
+    return ball.x > canvas.width - ball.r && ball.y > us.y && ball.y < us.y + paddleHeight;
+  }
+
+  const theyHit = () => {
+    return ball.x < ball.r && ball.y > them.y && ball.y < them.y + paddleHeight;
+  }
+
   const update = () => {
-    if (ball.x > canvas.width - ball.r || ball.x < ball.r) {
+    if (weHit() || theyHit()) {
       ball.dx = -ball.dx;
     }
 
